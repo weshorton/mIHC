57694cde21bfd99a45f43fb74827e05cbd821fca
Wes Horton <hortowe@ohsu.edu>
2023-09-25 13:20:17 -0700

    Add case for calcDensity when both metadata and data have Area column. Add more info to description in averageROIs

--- R/averageROIs.R
+++ R/averageROIs.R
@@ -12,6 +12,31 @@ averageROIs <- function(data_dt, sampleCol_v = "Sample_ID", prefix_v = "", split
   #' @param splitLabs_v character vector containing names of what to call results of sampleCol_v splitting
   #' @param by_v what to aggregate by. Default is Patient, but could be treatment or something if included in the name
   #' @param metaCols_v columns to exclude from aggregation
+  #' @details
+    #' The assignment of the column arguments depends on how the experiment was set up. The metadata for our experiments follow
+    #' the same general format, but samples can be related to each other in different ways, based on the experiment. Below are examples from previous projects
+    #' Future work should try to see if this can be standardized better, or maybe variable names changed to be more informative.
+    #' 
+    #' AMTEC 
+    #'   - A given sample can have one or multiple time points
+    #'   - Each timepoint can have multiple ROIs from its slide
+    #'   - ROIs are averaged per timepoint and reports are per sample, with one pie/bar per timepoint
+    #' Cell Death Panel
+    #'   - sampleCol_v = "Sample_ID"; ex: Nuclei_4xBLK26ROI01; Nuclei_4xBLK26ROI02
+    #'     - prefix_v = "Nueclei_"
+    #'     - split_v = "ROI"
+    #'     - splitLabs_v = c("Slide", "Sample")
+    #'     - A given patient/slide has multiple samples. Split the value to have the global ID (4xBLK26) and the sample ID (01, 02)
+    #'   - by_v = "Tumor"
+    #'     - This determines the replicates to average over
+    #'   - Each report is per sample (i.e. global ID), with one pie/bar containng the average values of the sample IDs
+    #'   - Also make cohort report
+    #' MdR01
+    #'   - Each ROI is unique
+    #'   - no averaging, only run this script to reformat
+    #'   - Pairs of samples format is: [0-9]{1}-[0-9]{1}[BTLR]; with [BLTR] distinguishing each pair.
+    #'   - Reports are per sample, with two bars/pies per plot (one for each member of pair)
+    #' 
   #' @return data.table with same columns. Rows are now summarized by by_v.
   #' @export
 
--- R/calcDensity.R
+++ R/calcDensity.R
@@ -17,10 +17,22 @@ calcDensity <- function(data_dt, meta_dt = NULL, areaCol_v = "Area", idCol_v = "
     cat(sprintf("No metadata provided. Checking data_dt for area column.\n"))
     if (!areaCol_v %in% colnames(data_dt)) stop("No metadata provided and data_dt does not have area column. Check arguments.\n")
 
-  ### If metadata, merge area column
+  ### If metadata, check for area column in data and meta.data
+  ### If both have one, make sure they're the same value
+  ### If not in data, make sure it is in meta.data. Then merge
   } else {
+    
+    if ("Area" %in% colnames(data_dt)) {
+      cat(sprintf("Metadata provided and data has Area column. Checking that they're equal.\n"))
+      checkArea_v <- all.equal(data_dt[[areaCol_v]], meta_dt[[areaCol_v]])
+      if (!is.logical(checkArea_v)) stop("Area columns in provided data and metadata don't match. Make sure they do!\n")
+      mergeCols_v <- c(idCol_v, areaCol_v)
+    } else {
+      if (!areaCol_v %in% colnames(meta_dt)) stop("Metadata doesn't have area column and neither does data. Can't get density without area!\n")
+      mergeCols_v <- idCol_v
+    }
 
-    data_dt <- merge(data_dt, meta_dt[,mget(c(idCol_v, areaCol_v)),drop = F], by = idCol_v, sort = F)
+    data_dt <- merge(data_dt, meta_dt[,mget(c(idCol_v, areaCol_v)),drop = F], by = mergeCols_v, sort = F)
 
   } # fi
 
