9ea72d7f4a82574739c94515bfcf50af580a5652
Wes Horton <hortowe@ohsu.edu>
2023-09-25 10:29:01 -0700

    Forgot to add docs and export readCSVorExcel

--- R/readCSVOrExcel.R
+++ R/readCSVOrExcel.R
@@ -1,4 +1,10 @@
 readCSVOrExcel <- function(file_v) {
+  #' Read csv of excel
+  #' @description Read in csv file or first worksheet of an excel workbook
+  #' @param file_v path to file to be read in
+  #' @return data.table
+  #' @export
+ 
   if (file_ext(file_v) == "csv") {
     out_dt <- fread(file_v)
   } else if (file_ext(file_v) %in% c("xlsx", "xls")) {
